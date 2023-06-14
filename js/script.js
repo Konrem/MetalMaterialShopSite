ac188a47916fafa64ca68e3bc66b45877544e741
Konrem <lord.kazakovv@gmail.com>
2023-06-14 20:01:11 +0300

    fix(catalog): move data table to separate file

--- js/script.js
+++ js/script.js
@@ -37,40 +37,6 @@ function playPauseAudio() {
 
 
 
-
-
-// Sample JSON data
-let jsonData = [
-    {
-        name: "Saurabh",
-        mark: "8н6",
-        size: "20",
-        unit: "Prayagraj",
-        cost: 2345
-    },
-    {
-        name: "Saurab4345h",
-        mark: "8нe6",
-        size: "20",
-        unit: "Prayagraj",
-        cost: 23645
-    },
-    {
-        name: "авпрпа",
-        mark: "85н6",
-        size: "2е0",
-        unit: "Pфіrayagraj",
-        cost: 2345
-    },
-    {
-        name: "Saurиатепратпамиabh",
-        mark: "85н6",
-        size: "240",
-        unit: "77",
-        cost: 2345
-    }
-];
-
 $(document).ready(function () {
     writeSelect(uniqMark(jsonData), "#select-mark", 1);
     writeSelect(uniqSize(jsonData), "#select-size", 2);
