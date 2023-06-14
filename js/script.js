291743df9eb60f9980f23f4573fde8ba266888ab
Konrem <lord.kazakovv@gmail.com>
2023-06-14 15:22:06 +0300

    fix(user-block): change all block and add audio

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@ $('.file-upload').change(function() {
     else $(this).prev().text('Виберіть файл');
 });
 
+
+const audio = new Audio('music.mp3');
+  
+let play=true;
+
+function playPauseAudio() {
+    if (play===true){
+        audio.play();
+        play=false;
+        $("#play").removeClass("d-block");
+        $("#play").addClass("d-none");
+        $("#pause").removeClass("d-none");
+        $("#pause").addClass("d-block");
+    } else {
+        audio.pause();
+        play=true;
+        $("#pause").removeClass("d-block");
+        $("#pause").addClass("d-none");
+        $("#play").removeClass("d-none");
+        $("#play").addClass("d-block");
+    }
+    audio.addEventListener('ended',function(){
+        audio.currentTime = 0;
+        $("#pause").removeClass("d-block");
+        $("#pause").addClass("d-none");
+        $("#play").removeClass("d-none");
+        $("#play").addClass("d-block");
+    }, false);
+}
